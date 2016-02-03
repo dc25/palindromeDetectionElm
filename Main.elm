a7ef8033d51b0d779da5ad989a68ac30579b5105
Dave Compton <davecompton7@gmail.com>
2016-02-03 18:47:00 +0000

    changed prompts and output from reverse to palindrome

--- Main.elm
+++ Main.elm
@@ -1,32 +1,37 @@
--- The import on the next line provides the reverse string 
--- functionality satisfying the rosettacode.org task description.
-import String exposing (reverse) 
-
--- The rest is fairly boilerplate code demonstrating
--- interactively that the reverse function works.
+import String exposing (reverse, length) 
 import Html exposing (Html, Attribute, text, div, input)
 import Html.Attributes exposing (placeholder, value, style)
 import Html.Events exposing (on, targetValue)
 import Signal exposing (Address)
 import StartApp.Simple exposing (start)
 
+-- The following function (copied from Haskell) satisfies the 
+-- rosettacode task description.
+is_palindrome x = x == reverse x
+
+-- The remainder of the code demonstrates the use of the function.
 main = start { model = "", view = view, update = update }
 
 update newStr oldStr = newStr
 
 view : Address String -> String -> Html
-view address forward =
+view address candidate =
   div []
     ([ input
-        [ placeholder "Enter a string to reverse."
-        , value forward
+        [ placeholder "Enter a string to check."
+        , value candidate
         , on "input" targetValue (Signal.message address)
         , myStyle
         ]
         []
      ] ++ 
-     [ let backward = reverse forward 
-       in div [ myStyle] [text backward] 
+     [ let testResult = 
+             is_palindrome candidate
+
+           statement = 
+             if testResult then "PALINDROME!!" else "not a palindrome"
+
+       in div [ myStyle] [text statement]
      ])
 
 myStyle : Attribute
